06dfefb5cc5701d135e3276e5ade06d6b0816b56
Perevoznikova Alisa <alice.castiel1@gmail.com>
2021-07-20 19:57:43 +0300

    tried working on importing from file 3

--- Android_App/app/src/main/java/natalia/doskach/audioorganizer/GetFileActivity.java
+++ Android_App/app/src/main/java/natalia/doskach/audioorganizer/GetFileActivity.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.nio.channels.FileChannel;
+import java.util.Objects;
 
 public class GetFileActivity extends AppCompatActivity {
 
@@ -65,13 +66,34 @@ public class GetFileActivity extends AppCompatActivity {
                 MainActivity.FileName = data.getData().getLastPathSegment();
                 textView2.setText(MainActivity.FileName);
                 audio = new Audio(MainActivity.FileName, "unknown", 10, MainActivity.FilePath, true);
-                File sourceFile = new File(MainActivity.FilePath);
-                File destDir = new File(getExternalFilesDir("").getAbsolutePath() + "/" + MainActivity.FileName + ".amr");
-                try {
-                    copyFileUsingStream(sourceFile, destDir);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+//                File original = new File(MainActivity.FilePath);
+//                File copied = new File(getExternalFilesDir("").getAbsolutePath());
+//                try (
+//                        InputStream in = new BufferedInputStream(
+//                                new FileInputStream(original));
+//                        OutputStream out = new BufferedOutputStream(
+//                                new FileOutputStream(copied))) {
+//
+//                    byte[] buffer = new byte[1024];
+//                    int lengthRead;
+//                    while ((lengthRead = in.read(buffer)) > 0) {
+//                        out.write(buffer, 0, lengthRead);
+//                        out.flush();
+//                    }
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+
+//                File sourceFile = new File(MainActivity.FilePath);
+//                File destDir = new File(getExternalFilesDir("").getAbsolutePath() + "/" + MainActivity.FileName + ".mp3");
+                copyFileOrDirectory(MainActivity.FilePath, getExternalFilesDir("").getAbsolutePath());
+//                try {
+//                    copyFileUsingStream(sourceFile, destDir);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
 //                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 //                        //copyAsset(MainActivity.FilePath, MainActivity.FileName);
 //                        copyDir();
@@ -102,16 +124,53 @@ public class GetFileActivity extends AppCompatActivity {
 //        finish();
     }
 
-    private static void copyFileUsingStream(File source, File dest) throws IOException {
-        try (InputStream is = new FileInputStream(source); OutputStream os = new FileOutputStream(dest)) {
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = is.read(buffer)) > 0) {
-                os.write(buffer, 0, length);
+    public static void copyFileOrDirectory(String srcDir, String dstDir) {
+
+        try {
+            File src = new File(srcDir);
+            File dst = new File(dstDir, src.getName());
+
+            if (src.isDirectory()) {
+
+                String[] files = src.list();
+                assert files != null;
+                for (String file : files) {
+                    String src1 = (new File(src, file).getPath());
+                    String dst1 = dst.getPath();
+                    copyFileOrDirectory(src1, dst1);
+
+                }
+            } else {
+                copyFile(src, dst);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!Objects.requireNonNull(destFile.getParentFile()).exists())
+            destFile.getParentFile().mkdirs();
+
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        try (FileChannel source = new FileInputStream(sourceFile).getChannel(); FileChannel destination = new FileOutputStream(destFile).getChannel()) {
+            destination.transferFrom(source, 0, source.size());
+        }
+    }
+//
+//    private static void copyFileUsingStream(File source, File dest) throws IOException {
+//        try (InputStream is = new FileInputStream(source); OutputStream os = new FileOutputStream(dest)) {
+//            byte[] buffer = new byte[1024];
+//            int length;
+//            while ((length = is.read(buffer)) > 0) {
+//                os.write(buffer, 0, length);
+//            }
+//        }
+//    }
+//
 //    private static void copyDir(String sourceDirName, String targetSourceDir) throws IOException {
 //        File folder = new File(sourceDirName);
 //
